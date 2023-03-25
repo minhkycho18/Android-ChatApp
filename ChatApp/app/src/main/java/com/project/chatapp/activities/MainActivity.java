b94aa5f6ed06af709d7aa9f6b08725c3368e3628
HaiTran <nguyenhai200902@gmail.com>
2023-03-25 12:15:37 +0700

    feat: Sign out, FCM token 25.03.2023

--- ChatApp/app/src/main/java/com/project/chatapp/activities/MainActivity.java
+++ ChatApp/app/src/main/java/com/project/chatapp/activities/MainActivity.java
@@ -3,7 +3,11 @@ package com.project.chatapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -11,16 +15,84 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.project.chatapp.activities.SignInActivity;
+import com.project.chatapp.databinding.ActivityMainBinding;
+import com.project.chatapp.utils.AppUtils;
+import com.project.chatapp.utils.Constants;
+import com.project.chatapp.utils.PreferenceManager;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+        getToken();
+        setListeners();
+
         mAuth = FirebaseAuth.getInstance();
         login("test@gmail.com","123456");
     }
+
+    private void setListeners() {
+        binding.imageSignOut.setOnClickListener(view -> signOut());
+    }
+
+    private void loadUserDetails() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
+    }
+
+    private void getToken() {
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token) {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused ->
+                        AppUtils.showToast("Token updated successfully", getApplicationContext()))
+                .addOnFailureListener(e ->
+                        AppUtils.showToast("Unable to update token", getApplicationContext()));
+
+    }
+
+    private void signOut() {
+        AppUtils.showToast("Signing Out...", getApplicationContext());
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                })
+                .addOnFailureListener(e ->
+                        AppUtils.showToast("Unable to Sign out", getApplicationContext()));
+
+    }
+
     @Override
     public void onStart() {
         super.onStart();
